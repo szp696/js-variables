a114abfa50e2cde97cd797991eb6f39718427177
szp696 <pszt000@gmail.com>
2021-11-24 11:53:44 +0100

    "tada"

--- main.js
+++ main.js
@@ -1,14 +1,28 @@
 let products = [
-    "Sampon", 
-    "Tusfürdő", 
     {
         category: "konyhai", 
         name: "turmix 2000", 
-        prise: 5000, 
-        inStock: true, 
+        price: 5000, 
+        stock: 1, 
         attachments: ["fej", "motor"]
+    },
+    {
+        category: "pipere", 
+        name: "Tusfürdő", 
+        price: 2000, 
+        stock: 3, 
+        attachments: ["valami", "masvalami"]
+    },
+    {
+        category: "pipere", 
+        name: "Sampon", 
+        price: 3000, 
+        stock: 8, 
+        attachments: ["fej", "shoulders"]
     }
 ];
+
+let customer = "Ádám";
 
 function main() {
     console.log("it works");
@@ -21,4 +35,43 @@ function main() {
         }
     }
 }
-window.addEventListener("load", main);
+function inventory() {
+    let stock = products[2].stock;
+    let price = products[2].price;
+    let value = stock * price;
+    let div = document.createElement("div");
+    div.innerText = products[2].name +" : " + value;
+    document.body.append(div);
+
+    stock = products[0].stock;
+    price = products[0].price;
+    value = stock * price;
+    div = document.createElement("div");
+    div.innerText = products[0].name +" : " + value;
+    document.body.append(div);
+
+    stock = products[1].stock;
+    price = products[1].price;
+    value = stock * price;
+    div = document.createElement("div");
+    div.innerText = products[1].name +" : " + value;
+    document.body.append(div);
+
+    stock = products[0].stock;
+    let available = stock >= 5;
+    div = document.createElement("div");
+    if (available) {
+        div.innerText = products[0].name +" elérhető ";
+    }
+    else {
+        div.innerText = products[0].name +" nem elérhető ";
+    }
+    document.body.append(div);
+
+    let special = customer === "Ádám";
+    if (!available &&  special){
+        div.innerText = div.innerText + "(De neki mégis)";
+    }
+}
+
+window.addEventListener("load", inventory);
